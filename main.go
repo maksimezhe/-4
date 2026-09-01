2d8012509a39efc6fac5229eda52b735d5cefc4b
maksimezhe <maksimezhe@gmail.com>
2026-09-01 18:58:13 +0500

    changed main

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Я сегодня изучил:")
+	fmt.Print("Я сегодня изучил: ")
 	fmt.Println("Основы языка Go")
 	fmt.Println("Мой любимый исполнитель: Темный Принц")
 	fmt.Println("Моя любимая песня: Темный Принц - one more time")
