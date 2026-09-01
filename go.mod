39eb31fd6b1f4f9e868d179457c8f13bf1ae1c69
maksimezhe <maksimezhe@gmail.com>
2026-09-01 18:54:42 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hometask4
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Я сегодня изучил:")
+	fmt.Println("Основы языка Go")
+	fmt.Println("Мой любимый исполнитель: Темный Принц")
+	fmt.Println("Моя любимая песня: Темный Принц - one more time")
+}
